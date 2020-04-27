bac2c0b4c5045318ffae9a26a6478c370202e4bc
lex <lex@users.noreply.github.com>
2020-04-27 23:45:17 +0300

    convert particle grid to an array

--- src/classes/FallingSandGame.ts
+++ src/classes/FallingSandGame.ts
@@ -1,32 +1,29 @@
 import Color from "./Color";
 import Particle from "./Particle";
 import ParticleType from "./ParticleType";
+import particleColors from './ParticleColors';
 
 export default class FallingSandGame {
   private width = 0;
   private height = 0;
   private framebuffer: Uint8Array;
-  private grid: Array<Array<Particle>>;
+  private inputBuffer: Array<Particle>;
 
   constructor(width: number, height: number, framebuffer: any) {
     this.width = width;
     this.height = height;
     this.framebuffer = framebuffer;
-    this.grid = this.createGrid(width, height);
+    this.inputBuffer = this.createBuffer(width, height);
   }
 
-  private createGrid(width: number, height: number) {
-    const grid = new Array(height);
+  private createBuffer(width: number, height: number) {
+    const buffer = new Array(width * height);
 
-    for (let y = 0; y < height; ++y) {
-      grid[y] = new Array(width);
-
-      for (let x = 0; x < width; ++x) {
-        grid[y][x] = new Particle(ParticleType.EMPTY);
-      }
+    for (let i = 0; i < width * height; ++i) {
+      buffer[i] = new Particle(ParticleType.EMPTY);
     }
 
-    return grid;
+    return buffer;
   }
 
   private updateFramebuffer(x: number, y: number, r: number, g: number, b: number): void {
@@ -37,10 +34,14 @@ export default class FallingSandGame {
     this.framebuffer[position + 2] = b;
   }
 
+  private getParticleAt(buffer: Array<Particle>, x: number, y: number): Particle {
+    return buffer[y * this.height + x];
+  }
+
   tick(): void {
     for (let y = this.height - 1; y >= 0; --y) {
       for (let x = 0; x < this.width; ++x) {
-        const current = this.grid[y][x] as Particle;
+        const current = this.getParticleAt(this.inputBuffer, x, y);
         const colors: Color = current.color;
 
         this.updateFramebuffer(x, y, colors.r, colors.g, colors.b);
@@ -59,15 +60,15 @@ export default class FallingSandGame {
         const atRightEdge = x === this.width - 1;
 
         const left =
-          !atLeftEdge ? this.grid[y][x - 1] : undefined;
+          !atLeftEdge ? this.getParticleAt(this.inputBuffer, x - 1, y) : undefined;
         const right =
-          !atRightEdge ? this.grid[y][x + 1] : undefined;
+          !atRightEdge ? this.getParticleAt(this.inputBuffer, x + 1, y) : undefined;
         const down =
-          !atBottom ? this.grid[y + 1][x] : undefined;
+          !atBottom ? this.getParticleAt(this.inputBuffer, x, y + 1) : undefined;
         const downLeft =
-          !atLeftEdge && !atBottom ? this.grid[y + 1][x - 1] : undefined;
+          !atLeftEdge && !atBottom ? this.getParticleAt(this.inputBuffer, x - 1, y + 1) : undefined;
         const downRight =
-          !atRightEdge && !atBottom ? this.grid[y + 1][x + 1] : undefined;
+          !atRightEdge && !atBottom ? this.getParticleAt(this.inputBuffer, x + 1, y + 1) : undefined;
 
         switch (current.type as number) {
           case ParticleType.SAND:
@@ -114,7 +115,8 @@ export default class FallingSandGame {
   createParticle(x: number, y: number, type: ParticleType): void {
     // todo check bounds
     try {
-      this.grid[y][x].type = type;
+      const particle = this.getParticleAt(this.inputBuffer, x, y);
+      particle.type = type;
     } catch (error) {
       console.error(error);
     }
